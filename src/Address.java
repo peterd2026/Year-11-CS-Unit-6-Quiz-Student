c5c3ec3c174b600d31bcaea5ffdbc5fa049af83f
peterdeng <peterd2026@student.cis.edu.hk>
2024-02-06 11:39:49 +0800

    unit 6 ide

--- src/Address.java
+++ src/Address.java
@@ -1,2 +1,38 @@
-public class Address {
+class Address {
+    private String district;
+    private String street;
+    private int streetNumber;
+
+    public Address(String district, String street, int streetNumber) {
+        this.district = district;
+        this.street = street;
+        this.streetNumber = streetNumber;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(int streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+    public String toString() {
+        return streetNumber + " " + street + ", " + district;
+    }
 }
